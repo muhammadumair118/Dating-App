f5ae7ef502bd91847fecd917a42da2252e248b66
John Doe <johndoe@example.com>
2020-03-22 00:37:47 +0500

    Security

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
+
+        [Required]
         public string Passaword { get; set; }
     }
 }
